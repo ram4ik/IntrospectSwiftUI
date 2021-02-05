044ae7bc28db972f292a5875f1b5701f678a07c5
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-05 20:13:52 +0200

    Initial Commit

---
+++ IntrospectSwiftUI/IntrospectSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntrospectSwiftUIApp.swift
+//  IntrospectSwiftUI
+//
+//  Created by Ramill Ibragimov on 05.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct IntrospectSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
