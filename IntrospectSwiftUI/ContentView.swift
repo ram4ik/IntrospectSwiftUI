7410304177c743b5a02cf7982c3afbcd3df237f2
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-02-05 20:52:17 +0200

    init

--- IntrospectSwiftUI/ContentView.swift
+++ IntrospectSwiftUI/ContentView.swift
@@ -6,11 +6,35 @@
 //
 
 import SwiftUI
+import Introspect
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        TabView {
+            NavigationView {
+                VStack {
+                    List {
+                        Text("Introspect")
+                        Text("Introspect")
+                        Text("Introspect")
+                    }
+                }
+                .introspectTableView(customize: { (tableView) in
+                    tableView.backgroundColor = UIColor.systemGreen
+                })
+                .navigationBarTitle("Introspect")
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .introspectNavigationController(customize: { (navigationController) in
+                navigationController.navigationBar.barTintColor = UIColor.systemGreen
+            })
+            .tabItem {
+                Image(systemName: "paperclip.badge.ellipsis")
+            }
+        }
+        .introspectTabBarController { (tabBarController) in
+            tabBarController.tabBar.barTintColor = UIColor.systemGreen
+        }
     }
 }
 
